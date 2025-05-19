9c6837211d3952ead05a267afd249c3a5a7a19f6
awaiz384 <awaiz.kareem@meridianit.com>
2025-05-19 16:39:37 +0530

    UM updated

--- src/main/java/com/databin_pg/api/Controlller/UserManagement/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.databin_pg.api.Controlller.UserManagement.repository;
-import com.databin_pg.api.Controlller.UserManagement.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    
-}
