ae7556d641bb3df2890d6817512417d518f6df5e
aminah <aminah.sultan@meridianit.com>
2025-06-05 12:52:30 +0530

    data loader problem resolved for hosting Backend

--- src/main/java/com/databin_pg/api/Repository/RoleRepository.java
+++ src/main/java/com/databin_pg/api/Repository/RoleRepository.java
@@ -1,8 +1,12 @@
 package com.databin_pg.api.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.databin_pg.api.Entity.Role;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {}
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByIdentifier(String identifier);
+}
 
